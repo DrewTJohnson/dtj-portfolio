4f4a31bcd80c3cc87130ea7fb4757c40e0d98abb
Drew Johnson <dtjohnson1995@gmail.com>
2024-08-30 08:59:37 -0400

    Force non-www

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects: async () => [
+        {
+            source: '/:path*',
+            has: [{ type: 'host', value: 'www.yourdomain.com' }],
+            destination: 'https://yourdomain.com/:path*',
+            permanent: true
+        }
+    ]
+};
 
 export default nextConfig;
